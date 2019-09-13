8bddfd920cf4d9dd8f36b8dac5e4b61d85665d7d
Sghulam22 <34138394+Sghulam22@users.noreply.github.com>
2019-09-12 23:56:55 -0400

    Update api1.php

--- api1.php
+++ api1.php
@@ -23,6 +23,7 @@
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     print_r($results);
     $arr=$results[0]["words"];
+    print_r($arr);
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
     header('HTTP/1.1 200 OK');
