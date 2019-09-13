9427cca3ec7988441536e921d3f45056c36d31ca
Sghulam22 <34138394+Sghulam22@users.noreply.github.com>
2019-09-13 13:31:45 -0400

    Update api1.php

--- api1.php
+++ api1.php
@@ -12,7 +12,7 @@
     $temp="AEESSSW";
     $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";
     $query2 = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";
-    $query3 = 'SELECT rack, words FROM racks WHERE rack="$temp"';
+    $query3 = 'SELECT rack, words FROM racks WHERE rack=$temp';
     
     //this next line could actually be used to provide user_given input to the query to 
     //avoid SQL injection attacks 
