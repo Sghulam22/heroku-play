15d89c83579966d572c8b4a902e7fbfff62b7bd4
Sghulam22 <34138394+Sghulam22@users.noreply.github.com>
2019-09-14 18:50:09 -0400

    Update api.php

--- api.php
+++ api.php
@@ -1,5 +1,4 @@
 <?php
-
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
@@ -8,15 +7,8 @@
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
     // ordering by rank, etc.
-    $myrack ="bail";
-    $mywords=[];
-
-    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";  
-    $query2 = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";  
-  //   $query = 'SELECT rack, words FROM racks WHERE rack="potato"';
+    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
     
-
-    print_r($query);    
     //this next line could actually be used to provide user_given input to the query to 
     //avoid SQL injection attacks
     $statement = $dbhandle->prepare($query);
@@ -28,9 +20,6 @@
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     
-    print_r($results);
-    
-    
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
     header('HTTP/1.1 200 OK');
@@ -38,5 +27,4 @@
     header('Content-Type: application/json');
     //this creates json and gives it back to the browser
     echo json_encode($results);
-
-?>
+?> 
