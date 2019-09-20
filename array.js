418027fd94c6b13fdda8c95f817c0b320659cba9
Sghulam22 <34138394+Sghulam22@users.noreply.github.com>
2019-09-19 22:17:35 -0400

    Update array.js

--- array.js
+++ array.js
@@ -1,6 +1,5 @@
 	var length;
 	var arr=[];
-	var recheck = [];
 	var string="";
   
 	function loadrack() {	
@@ -14,10 +13,6 @@
 		
 		var temp= array[0];
 		var x=temp.length;
-		
-		//document.getElementById("f").innerHTML = array;
-		
-		//var res = temp.substr(3, x-3);
 		var answers_array = new Array(array.length-1);
 		
 		var count=1;
@@ -33,11 +28,9 @@
 			x=temp.length;
 			res=temp.substr(1, x-2);
 			answers_array[count]=res;
-			count++;
-			
+			count++;	
 		}
     		arr=answers_array;
-		recheck=arr;
 		document.getElementById("results").innerHTML = arr;
   	  });
  	}
@@ -46,39 +39,17 @@
 	
 		var x=document.getElementById("input").value;
 		var i=0;
-		var check=0;
 		while(i<arr.length)
 		{
 		 	if(x==arr[i])
 			{
 				string=string+"*"+arr[i]+"*  ";
 				arr[i]="erased/null";
-				check=1;
 				alert("CORRECT!!");
-				
 			}
 			i++;
 		 }
-		i=0;
 
-		if (check==0)
-		{
-		while(i<recheck.length)
-		  {
-			if(x==recheck[i])
-			{
-				check=1;
-			}
-			i++;
-			}
-			if(check==1)
-			{
-				alert("you have already entered this word");
-			}
-			else if(check ==0){
-				alert("incorrect try again please");
-			}
-		}     
 		document.getElementById("results").innerHTML = string;
 		    	
 	}
